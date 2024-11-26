5c4444b09887db4e17cc3783af878053f9fa018e
pernebay-arailym <pernebayarailym@gmail.com>
2024-11-26 15:17:10 +0100

    plotted bar for top count loc & populationJan

--- outputs/animation.py
+++ outputs/animation.py
@@ -7,4 +7,4 @@ pop_data_1950 = pop_data[pop_data['Time'] == 1950]
 
 top_countries = pop_data_1950.nlargest(10, 'TPopulation1Jan')
 
-top_countries
+plt.barh(top_countries['Location'], top_countries['TPopulation1Jan'])
